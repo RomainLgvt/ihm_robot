d62fe2f1fb565b1c830f66f4dc77e2336372f97a
RomainLgvt <ligavant@lacroixrouge-brest.fr>
2018-11-05 16:05:47 +0100

    Création de l'ihm de base: 4 boutons + 2 input text pour les 2 moteurs

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>mon site web</title>
+</head>
+<body>
+<?php
+	echo "Nous sommes aujourd'hui" . date("l").".";
+	
+?>
+
+
+	<div id="titre1" class="titre" style="border:10px solid">
+<h1>Mon premier site web</h1>
+	</div>
+	<div style="border:10px solid">
+<p>Ceci est un paragraphe</p>
+<a href=#>mon premier lien</a>
+<img src=""></img>
+</div>
+</body>
+</html>
